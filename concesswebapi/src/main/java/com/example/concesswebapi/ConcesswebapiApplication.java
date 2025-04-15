524eaf3568db1d56676f7702e9b88d8f8985da04
LeonardoCCR <leonardocesar01.12@gmail.com>
2025-04-15 08:02:00 -0300

    arquivos iniciais do back-end

---
+++ concesswebapi/src/main/java/com/example/concesswebapi/ConcesswebapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.concesswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesswebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesswebapiApplication.class, args);
+	}
+
+}
---
+++ concesswebapi/src/test/java/com/example/concesswebapi/ConcesswebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concesswebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesswebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

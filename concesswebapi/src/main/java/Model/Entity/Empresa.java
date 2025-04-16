a53c998b364663ab2daa64cac8a76496fa2fc22c
RodrigoCGuedes <rodrigocguedesbsi@gmail.com>
2025-04-16 19:02:11 -0300

    Adicionando as classes filhas de pessoa Juridica.

---
+++ concesswebapi/src/main/java/Model/Entity/Empresa.java
@@ -0,0 +1,4 @@
+package Model.Entity;
+
+public class Empresa extends PessoaJuridica{
+}
---
+++ concesswebapi/src/main/java/Model/Entity/PessoaJuridica.java
@@ -0,0 +1,7 @@
+package Model.Entity;
+
+public class PessoaJuridica {
+
+    private String razao_social;
+    private String cnpj;
+}
